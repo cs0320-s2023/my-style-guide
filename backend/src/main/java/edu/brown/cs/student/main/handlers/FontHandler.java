07dd0716d51a4b79949fa37a8a5a45a183e2433b
Isaac Jenemann <isaac_jenemann@brown.edu--global user.name jenemani>
2023-05-03 23:19:02 -0400

    font generator

---
+++ backend/src/main/java/edu/brown/cs/student/main/handlers/FontHandler.java
@@ -0,0 +1,128 @@
+package edu.brown.cs.student.main.handlers;
+import com.squareup.moshi.*;
+import edu.brown.cs.student.main.Constants;
+import edu.brown.cs.student.main.jsonUtils.Serializer;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class FontHandler implements Route {
+
+    private final String FONT_API_KEY = Constants.FONT_KEY;
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            return this.fontInfo("Abel");
+        } catch (Exception e) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("result", "error_bad_json");
+            map.put("message", "unknown request" + e);
+            return Serializer.serializeFailure(map);
+        }
+    }
+
+
+    public String fontify(Request request) throws Exception {
+        String url = "https://api.openai.com/v1/completions";
+        String descriptor = request.queryParams("adj");
+        System.out.println(descriptor);
+        HttpURLConnection gpt = (HttpURLConnection) new URL(url).openConnection();
+
+
+        gpt.setRequestMethod("POST");
+        gpt.setRequestProperty("Content-Type", "application/json");
+        gpt.setRequestProperty("Authorization", "Bearer "+ Constants.GPT_KEY);
+
+        Map<String,Object > data = new HashMap<>();
+        data.put("model", "text-davinci-003");
+        data.put("prompt", "A google font for the header of a " + descriptor + " website:\n\n google-font: #");
+        data.put("max_tokens", 200);
+        data.put("temperature", .6);
+
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Map<String, Object>> jsonAdapter = moshi.adapter(Types.newParameterizedType(Map.class, String.class, Object.class));
+        String jsonData = jsonAdapter.toJson(data);
+
+        gpt.setDoOutput(true);
+        gpt.getOutputStream().write(jsonData.getBytes());
+        String jsonResponse = new BufferedReader(new InputStreamReader(gpt.getInputStream())).readLine();
+
+        JsonAdapter<Map<String, List<Map<String, Object>>>> gptAdapter = moshi.adapter(Types.newParameterizedType(Map.class, String.class, Object.class));
+        Map<String, List<Map<String, Object>>> responseMap = gptAdapter.fromJson(jsonResponse);
+        List<Map<String, Object>> choicesList = responseMap.get("choices");
+        Map<String, Object> firstChoice = choicesList.get(0);
+        String output = (String) firstChoice.get("text");
+        return output;
+    }
+
+    public Map<String, String> fontInfo(String gptFont) throws IOException {
+        Map<String,String> fontInfo = new HashMap<>();
+        String font = gptFont.replace(" ","+");
+        String url = "https://www.googleapis.com/webfonts/v1/webfonts?key="+  Constants.FONT_KEY +"&family="+font;
+
+
+        try{
+            HttpURLConnection fontConnection = (HttpURLConnection) new URL(url).openConnection();
+            fontConnection.setRequestMethod("GET");
+
+            Moshi moshi = new Moshi.Builder().build();
+            JsonAdapter<FontResponse> adapter = moshi.adapter(FontResponse.class);
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(fontConnection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = bReader.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            bReader.close();
+            fontConnection.disconnect();
+
+            System.out.println(response);
+            FontResponse fontResponse = adapter.fromJson(response.toString());
+            System.out.println("b");
+
+
+            assert fontResponse != null;
+            fontInfo.put(font, fontResponse.items.get(0).category);
+
+        } catch (Exception e) {
+            fontInfo.put("Error", "No such font found");
+        }
+
+        return fontInfo;
+    }
+
+    public static class FontResponse {
+      public String kind;
+      public List<Font> items;
+
+      public static class Font {
+          public String family;
+          public List<String> variants;
+          public List<String> subsets;
+          public String version;
+          public String lastModified;
+          public Files files;
+          public String category;
+          public String kind;
+          public String menu;
+
+          public static class Files {
+              public String regular;
+          }
+      }
+  }
+}
