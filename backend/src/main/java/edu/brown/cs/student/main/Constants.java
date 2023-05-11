05ab4399ed98c47fc40fdb78f2d4558deb68fc9c
Isaac Jenemann <isaac_jenemann@brown.edu--global user.name jenemani>
2023-05-11 10:09:34 -0400

    gitignore

--- backend/src/main/java/edu/brown/cs/student/main/Constants.java
+++ backend/src/main/java/edu/brown/cs/student/main/Constants.java
@@ -2,5 +2,5 @@ package edu.brown.cs.student.main;
 
 public class Constants {
     public static final String FONT_KEY = "AIzaSyCj_Mhke0zUczf0viyXaHvAgrwn_ww3288";
-    public static final String GPT_KEY = "testing123";
+    public static final String GPT_KEY = "abcdefg";
 }
